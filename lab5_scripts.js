e9f1470b4410f623ee43e4e04d2a9f01f14be32f
euxefkarl <blancadaeu@gmail.com>
2026-03-23 20:22:52 +0800

    search student

--- lab5_scripts.js
+++ lab5_scripts.js
@@ -117,68 +117,6 @@ function validateForm(name, age, email) {
   return errors;
 }
 
-//form code
-
-const students = [];
-
-function add_student(event) {
-  event.preventDefault();
-  const name = document.getElementById("name").value;
-  const age = document.getElementById("age").value;
-  const email = document.getElementById("email").value;
-  const course = document.getElementById("course").value;
-  const studentID = getStudentID();
-  const student = {
-    name: name,
-    age: age,
-    email: email,
-    course: course,
-    studentID: studentID,
-  };
-  const errors = validateForm(name, age, email);
-
-  if(errors.length > 0) {
-    alert("Form submission failed:\n" + errors.join("\n"));
-    return;
-  }
-  
-  students.push(student);
-  alert("Student added: " + name + ", ID: " + studentID);
-  document.getElementById("studentForm").reset();
-}
-
-function getStudentID() {
-  const random = Math.floor(Math.random() * 90000) + 10000;
-  const studentID = "2024" + random.toString();
-  for (let i = 0; i < students.length; i++) {
-    if (students[i].studentID === studentID) {
-      return getStudentID();
-    }
-  }
-  return studentID;
-}
-
-function validateForm(name, age, email) {
-  let errors = [];
-
-  if (name.length <= 5) {
-    errors.push("Name must be more than 5 characters.");
-  }
-  if (!name.includes(" ")) {
-    errors.push("Name must contain a space.");
-  }
-  if (isNaN(age)) {
-    errors.push("Age must be a number.");
-  }
-  if (age < 19 || age > 98) {
-    errors.push("Age must be between > 18 and  < 99.");
-  }
-  if (!email.endsWith("@up.edu.ph")) {
-    errors.push("Email must end with @up.edu.ph.");
-  }
-  return errors;
-}
-
 // search student
 
 function find_student(event){
